b05a6da4af982c9541c666d741027592f7dd4593
Bianca Gualter <bncgualter@gmail.com>
2021-02-02 02:14:42 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  root: {
+    
+  }
+})
 
 function App() {
+  const theme = createMuiTheme({
+
+  })
+
+  const classes = useStyles()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
