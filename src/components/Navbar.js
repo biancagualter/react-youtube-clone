1b8c96ddfe5fcc379078fa1603f3e5834b8bc416
Bianca Gualter <bncgualter@gmail.com>
2021-02-05 04:31:52 -0300

    customize navbar components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Toolbar, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText  } from '@material-ui/core'
+import { Toolbar, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, Box, Typography, Button, ListSubheader  } from '@material-ui/core'
 import useStyles from '../styles/index'
+
+import { HomeSharp, Subscriptions, Whatshot, VideoLibrary, History, AccountCircle, AddCircle, YouTube, LiveTv } from '@material-ui/icons'
+
 
 const Navbar = () => {
     const classes = useStyles()
@@ -15,22 +18,142 @@ const Navbar = () => {
             >
                 <Toolbar />
                 <div className={classes.drawerContainer}>
-                    <List>
-                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                        <ListItem button key={text}>
-                            {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
-                            <ListItemText primary={text} />
+                <Divider className={classes.divider}/>
+                    <List classes={{root: classes.list}}>
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon classes={{root: classes.listIcon}}>{<HomeSharp />}</ListItemIcon>
+                            <ListItemText classes={{
+                                primary: classes.listItemText
+                            }} primary={'Início'} />
                         </ListItem>
-                        ))}
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon classes={{root: classes.listIcon}}>{<Whatshot />}</ListItemIcon>
+                            <ListItemText classes={{
+                                primary: classes.listItemText
+                            }} primary={'Em alta'} />
+                        </ListItem>
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon classes={{root: classes.listIcon}}>{<Subscriptions />}</ListItemIcon>
+                            <ListItemText classes={{
+                                primary: classes.listItemText
+                            }} primary={'Inscrições'} />
+                        </ListItem>
                     </List>
                     <Divider />
-                    <List>
-                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                        <ListItem button key={text}>
-                            {/* <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon> */}
-                            <ListItemText primary={text} />
+                    <List classes={{root: classes.list}}>
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon classes={{root: classes.listIcon}}>{<VideoLibrary />}</ListItemIcon>
+                            <ListItemText classes={{
+                                primary: classes.listItemText
+                            }} primary={'Biblioteca'} />
                         </ListItem>
-                        ))}
+                        <ListItem button classes={{root: classes.listItem}}>
+                            <ListItemIcon classes={{root: classes.listIcon}}>{<History />}</ListItemIcon>
+                            <ListItemText classes={{
+                                primary: classes.listItemText
+                            }} primary={'Histórico'} />
+                        </ListItem>
+                    </List>
+                    <Divider />
+
+                    <Box p={4} className={classes.box}>
+                        <Typography variant='body3'>
+                            Faça login para curtir vídeos, comentar e se inscrever.
+                        </Typography>
+                        <Box mt={2}>
+                            <Button 
+                                variant='outlined'
+                                color='secondary'
+                                startIcon={<AccountCircle />}
+                            >
+                                Fazer login
+                            </Button>
+                        </Box>
+                    </Box>
+                    <Divider />
+                    <List
+                        component='nav'
+                        aria-labelledby='list-subheader'
+                        subheader={
+                            <ListSubheader id='list-subheader' className={classes.subheader}>
+                                    O Melhor do Youtube
+                            </ListSubheader>
+                        }
+                    />
+                    <List className={classes.listMore}>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Música'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Esportes'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Jogos'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Filmes'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Notícias'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Ao vivo'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Destaques'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><AddCircle /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Vídeos 360'} />
+                        </ListItem>
+                    </List>
+                    <Divider />
+
+                    <List
+                        component='nav'
+                        aria-labelledby='list-subheader'
+                        subheader={
+                            <ListSubheader id='list-subheader' className={classes.subheader}>
+                                    Mais do Youtube
+                            </ListSubheader>
+                        }
+                    />
+                    <List className={classes.listMore}>
+                        <ListItem button>
+                                <ListItemIcon><YouTube /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Youtube Premium'} />
+                        </ListItem>
+                        <ListItem button>
+                                <ListItemIcon><LiveTv /></ListItemIcon>
+                                <ListItemText classes={{
+                                    primary: classes.listItemText
+                                }}primary={'Ao vivo'} />
+                        </ListItem>
                     </List>
                 </div>
             </Drawer> 
--- src/styles/index.js
+++ src/styles/index.js
@@ -5,33 +5,65 @@ import { makeStyles} from '@material-ui/core'
       height: '100vh'
     },
     appBar: {
-        boxShadow: 'none',
-        zIndex: theme.zIndex.drawer + 1,
+      boxShadow: 'none',
+      zIndex: theme.zIndex.drawer + 1,
+      height: 56
     },
     toolbar: {
-        paddingBottom: 10,
+      paddingBottom: 10
     },
     logo: {
-       height: 18, 
-       paddingRight: 10,
+      height: 18, 
+      paddingRight: 10
+    },
+    divider: {
+      marginTop: -8
     },
     drawer: {
-        width: 240,
-        flexShrink: 0,
+      width: 240,
+      flexShrink: 0
       },
-      drawerPaper: {
-        width: 240,
-      },
+    drawerPaper: {
+      width: 240,
+      borderRight: 'none'
+    },
     menuIcon: {
-        paddingRight: theme.spacing(3),
-        paddingLeft: 10,
+      paddingRight: theme.spacing(3),
+      paddingLeft: 10
     },
     icons: {
-        paddingRight: theme.spacing(2),
+      paddingRight: theme.spacing(2),
     },
     grow: {
-        flexGrow: 1
-    }
+      flexGrow: 1
+    },
+    listItemText: {
+      fontSize: 14
+    },
+    list: {
+      paddingTop: 11,
+      paddingBottom: 13
+    },
+    listItem: {
+      paddingTop: 6,
+      paddingBottom: 6,
+      paddingLeft: 24
+    },
+    listIcon: {
+      marginRight: -10
+    },
+    box: {
+      paddingTop: 23,
+      paddingBottom: 23
+    },
+    subheader: {
+      textTransform: 'uppercase',
+      fontWeight: 'bold'
+    },
+    listMore: {
+      marginTop: -16
+    },
+
   }))
 
   export default useStyles
